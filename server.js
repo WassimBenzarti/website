41065b1826a184373db73cad56d95ea6c48800c5
Wassim Benzart <wass11121996@gmail.com>
2016-07-05 14:49:09 +0100

    Hello world

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var http = require('http');
+var express = require('express');
 
+var app = express();
 
+app.use('/',function(req,res,next){
+    res.send('<h1>This is MY express App</h1>');
+
+    next();
+});
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
