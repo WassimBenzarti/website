4c719b8a90666eaaffb8728638119756395cf830
Wassim Benzart <wass11121996@gmail.com>
2016-07-05 14:29:51 +0100

    Hello world

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+function reqHandler(req, res){
+    console.log('requested ', req.url, 'on host ',req.host);
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.write('<h1>Hello GUYS !</h1>');
+    res.end();
+}
+
+
+var server = http.createServer(reqHandler).listen(port,ip,function(){
+    console.log('My server is running on port %s : %s ',ip,port);
+});
